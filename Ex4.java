bd818db44161cc646c49121576e4fbff67d5b19e
s1280234 <86813227+s1280234@users.noreply.github.com>
2021-07-02 11:41:21 +0900

    Initial revision

---
+++ Ex4.java
@@ -0,0 +1,10 @@
+public class Ex4{
+public static void main(String[] args){        
+        int value1 = (int)(Math.random() * 6) + 1;
+        System.out.println("Die1: " +value1);
+        int value2 = (int)(Math.random() * 6) + 1;
+        System.out.println("Die2: " +value2);
+        int total = value1 + value2;
+        System.out.println("Total value: " +total);
+        }
+    }
