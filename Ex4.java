70123f349b4366aa40c9265374e801c0d68fcecf
s1280234 <86813227+s1280234@users.noreply.github.com>
2021-07-02 12:01:54 +0900

    Greetings added

--- Ex4.java
+++ Ex4.java
@@ -1,10 +1,16 @@
-public class Ex4{
-public static void main(String[] args){        
-        int value1 = (int)(Math.random() * 6) + 1;
-        System.out.println("Die1: " +value1);
-        int value2 = (int)(Math.random() * 6) + 1;
-        System.out.println("Die2: " +value2);
-        int total = value1 + value2;
-        System.out.println("Total value: " +total);
+import java.util.Scanner;
+        public class Ex4{
+        public static void main(String[] args){        
+        Scanner scanner = new Scanner(System.in);
+         System.out.println("What is your name?");
+         String input_text = scanner.nextLine();
+         System.out.println("Hello," +input_text+ "!");
+
+         int value1 = (int)(Math.random() * 6) + 1;
+         System.out.println("Die1: " +value1);
+         int value2 = (int)(Math.random() * 6) + 1; 
+         System.out.println("Die2: " +value2);
+         int total = value1 + value2;
+          System.out.println("Total value: " +total);
         }
     }
